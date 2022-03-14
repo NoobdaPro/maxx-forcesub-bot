952c07fced1cf8674039688ecf6fc14a3e46ea99
NoobdaPro <98887725+NoobdaPro@users.noreply.github.com>
2022-03-14 08:29:53 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -40,7 +40,7 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = "https://t.me/MaxxBots"
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/MaxxBots")],
+            [InlineKeyboardButton(text = 'MY BOSS', url="https://t.me/HARSHU_XD")],
             [InlineKeyboardButton(text = 'Feature Request', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]
